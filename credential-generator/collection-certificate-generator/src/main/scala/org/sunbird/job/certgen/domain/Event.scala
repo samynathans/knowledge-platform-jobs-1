ce788bbf4f3091a402d515677ac3e69db1904613
Juhi Agarwal <juhiagl8@gmail.com>
2022-09-02 12:44:29 +0530

    changes for adding location

--- credential-generator/collection-certificate-generator/src/main/scala/org/sunbird/job/certgen/domain/Event.scala
+++ credential-generator/collection-certificate-generator/src/main/scala/org/sunbird/job/certgen/domain/Event.scala
@@ -63,6 +63,6 @@ class Event(eventMap: java.util.Map[String, Any], partition: Int, offset: Long) 
   def orgName: String = readOrDefault[String]("edata.orgName", "")
   def country: String = readOrDefault[String]("edata.country", "")
   def state: String = readOrDefault[String]("edata.state", "")
-  def district: String = readOrDefault[String]("edata.districtgit", "")
+  def district: String = readOrDefault[String]("edata.district", "")
 
 }
